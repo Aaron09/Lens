15554a8496d57343e8a81edf47b52f1eeeda9881
Aaron Green <aaron.green281@gmail.com>
2017-05-06 15:37:40 -0500

    init commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <h4> Hi! My name is Aaron. </h4>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('root')
+);
