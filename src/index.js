85a5e0cc81315c1cf8d5f1d087984c82fa78c9d0
Aaron Green <aaron.green281@gmail.com>
2017-05-08 23:27:59 -0500

    not spread out

--- src/index.js
+++ src/index.js
@@ -9,32 +9,26 @@ import "./index.css";
 class Home extends React.Component {
   render() {
     return (
-      <div className="navigation">
-        <div className="homeButton">
-          <Nav className="homeNav" bsStyle="pills">
-          <NavItem href="/">
-            <span className="homeNavButton">SmartEdit</span>
-          </NavItem>
-          </Nav>
-        </div>
-        <Nav className="navigationBar" bsStyle="pills" pullRight>
-          <NavItem href="/store"> 
-            <span className="storeNavButton">Store</span>
-          </NavItem>
-          <NavItem href="/edit"> 
-            <span className="editNavButton">Edit</span>
-          </NavItem>
-          <NavItem href="/share"> 
-            <span className="shareNavButton">Share</span>
-          </NavItem>
-          <NavItem href="/login"> 
-            <span className="loginNavButton">Login</span>
-          </NavItem>
-          <NavItem href="/signup"> 
-            <span className="signUpNavButton">Sign Up</span>
-          </NavItem>
-        </Nav>
-      </div>
+      <Nav className="navigationBar" bsStyle="pills">
+        <NavItem href="/">
+          <span className="homeNavButton">SmartEdit</span>
+        </NavItem>
+        <NavItem href="/store"> 
+          <span className="storeNavButton">Store</span>
+        </NavItem>
+        <NavItem href="/edit"> 
+          <span className="editNavButton">Edit</span>
+        </NavItem>
+        <NavItem href="/share"> 
+          <span className="shareNavButton">Share</span>
+        </NavItem>
+        <NavItem href="/login"> 
+          <span className="loginNavButton">Login</span>
+        </NavItem>
+        <NavItem href="/signup"> 
+          <span className="signUpNavButton">Sign Up</span>
+        </NavItem>
+      </Nav>
     );
   }
 }
