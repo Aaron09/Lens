72e8863c89e23ae3d3ca187ee0c74ce8b0fb1a00
Aaron Green <aaron.green281@gmail.com>
2017-05-08 21:57:10 -0500

    started

--- src/index.js
+++ src/index.js
@@ -1,64 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from 'react-bootstrap';
+import { Nav } from "react-bootstrap";
+import { NavItem } from "react-bootstrap";
+
+import "./index.css";
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <div>
-        <img 
-        src="https://upload.wikimedia.org/wikipedia/commons/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg" 
-        className="everestImage"
-        />
-        <Title />
-        <NameInput />
+      <div className="navigation">
+        <div className="homeButton">
+          <Nav className="homeNav" bsStyle="pills">
+          <NavItem href="/">
+            <span className="homeNavButton">SmartEdit</span>
+          </NavItem>
+          </Nav>
+        </div>
+        <Nav className="navigationBar" bsStyle="pills" pullRight>
+          <NavItem href="/store"> 
+            <span className="storeNavButton">Store</span>
+          </NavItem>
+          <NavItem href="/edit"> 
+            <span className="editNavButton">Edit</span>
+          </NavItem>
+          <NavItem href="/share"> 
+            <span className="shareNavButton">Share</span>
+          </NavItem>
+          <NavItem href="/login"> 
+            <span className="loginNavButton">Login</span>
+          </NavItem>
+          <NavItem href="/signup"> 
+            <span className="signUpNavButton">Sign Up</span>
+          </NavItem>
+        </Nav>
       </div>
-    );
-  }
-}
-
-class Title extends React.Component {
-  render() {
-    return (
-      <div className="title"> 
-        <h4> Hi! My name is Aaron. </h4>
-        <h7> What is your name? </h7>
-      </div>
-    );
-  }
-}
-
-class NameInput extends React.Component {
-  constructor(props, imageTitle) {
-    super(props);
-    this.state = {value: ""};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="nameInputForm">
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
     );
   }
 }
@@ -67,5 +43,6 @@ class NameInput extends React.Component {
 
 ReactDOM.render(
   <Home />,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
