5e19ff7140b2736924464504e2abce6b08700df7
Aaron Green <aaron.green281@gmail.com>
2017-05-10 20:56:11 -0500

    finish front page

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ import { ControlLabel } from "react-bootstrap";
 
 import "./index.css";
 
+/**
+ * Background Image
+ */
 class BackgroundImage extends React.Component {
   render() {
     return (
@@ -20,6 +23,9 @@ class BackgroundImage extends React.Component {
   }
 }
 
+/**
+ * Nav Bar
+ */
 class Navigation extends React.Component {
   render() {
     return (
@@ -31,14 +37,11 @@ class Navigation extends React.Component {
             </li>          
           </div>
           <div className="rightSideButtons">
-            <li className="storeNavButton">
-              <a href="/store"> Store </a>
+            <li className="streamNavButton"> 
+              <a href="/stream"> Stream </a>
             </li>
-            <li className="editNavButton"> 
-              <a href="/edit"> Edit </a>
-            </li>
-            <li className="shareNavButton">
-              <a href="/share"> Share </a>
+            <li className="settingsNavButton">
+              <a href="/settings"> Settings </a>
             </li>
             <li className="loginNavButton">
               <a href="/login"> Login </a>
@@ -53,6 +56,9 @@ class Navigation extends React.Component {
   }
 }
 
+/**
+ * Product Slogan
+ */
 class Slogan extends React.Component {
   render() {
     return (
@@ -63,21 +69,73 @@ class Slogan extends React.Component {
   }
 }
 
-class LoginForm extends React.Component {
+/*class LoginForm extends React.Component {
   render() {
     return (
       <Form horizontal className="loginForm">
         <FormGroup controlId="formHorizontalEmail">
-          <Col sm={10}>
+          <Col >
             <FormControl type="email" placeholder="Email" />
           </Col>
         </FormGroup>
         <FormGroup controlId="formHorizontalPassword">
-          <Col sm={10}>
+          <Col >
             <FormControl type="password" placeholder="Password" />
           </Col>
         </FormGroup>
+        <FormGroup>
+          <Col smOffset={7}>
+            <Button type="submit">
+              Login
+            </Button>
+          </Col>
+        </FormGroup>
       </Form>
+    );
+  }
+}*/
+
+/**
+ * All information in the about section
+ */
+class AboutInformation extends React.Component {
+  render() {
+    return (
+      <div className="aboutInformation">
+        <ProductDescription />
+        <PlatformIntegrationInformation />
+      </div>
+    );
+  }
+}
+
+/**
+ * All platform informations
+ */
+class PlatformIntegrationInformation extends React.Component {
+  render() {
+    return (
+      <ul className="platformIntegrationInformation">
+        <li className="facebookInfo">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/F_icon.svg/2000px-F_icon.svg.png" />
+        </li>
+        <li className="twitterInfo">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Twitter_Logo_White_On_Blue_Circular.svg/2000px-Twitter_Logo_White_On_Blue_Circular.svg.png" />
+        </li>
+        <li className="instagramInfo">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2000px-Instagram_logo_2016.svg.png" />
+        </li>
+      </ul>
+    );
+  }
+}
+
+class ProductDescription extends React.Component {
+  render() {
+    return (
+      <div className="productDescription">
+        <h7> An integrated content viewer, showing you exactly what you want to see. </h7>
+      </div>
     );
   }
 }
@@ -89,7 +147,7 @@ class Home extends React.Component {
         <BackgroundImage />
         <Navigation />
         <Slogan />
-        <LoginForm />
+        <AboutInformation />
       </div>
     );
   }
